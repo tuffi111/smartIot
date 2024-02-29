d6cd6d5b1d0f87735025c36eebc55c72ed9c0f42
Thomas Schweden <thomas.schweden@meetago.com>
2024-02-29 05:36:29 +0100

    refactor models

--- server/resources/frontend/src/services/browserSettings.js
+++ server/resources/frontend/src/services/browserSettings.js
@@ -1,36 +1,10 @@
 import {BrowserSettingsModel} from "@/models/BrowserSettingsModel.js";
-import {SessionDataModel} from "@/models/SessionDataModel.js";
-import {LocalDataModel} from "@/models/LocalDataModel.js";
-import {CookieDataModel} from "@/models/CookieDataModel.js";
 
 let browserSettings;
-let sessionData;
-let cookieData;
 
 export function useBrowserSettings(){
     if(!browserSettings){
         browserSettings = new BrowserSettingsModel()
     }
     return browserSettings;
-}
-
-export function useLocalModel(){
-    if(!sessionData){
-        sessionData = new LocalDataModel()
-    }
-    return sessionData;
-}
-
-export function useSessionModel(){
-    if(!sessionData){
-        sessionData = new SessionDataModel()
-    }
-    return sessionData;
-}
-
-export function useCookieModel(){
-    if(!cookieData){
-        cookieData = new CookieDataModel()
-    }
-    return cookieData;
 }
--- server/resources/frontend/src/views/Settings.vue
+++ server/resources/frontend/src/views/Settings.vue
@@ -1,16 +1,19 @@
 <script setup>
 import Default from "@/layouts/Default.vue";
-import {useSessionModel, useBrowserSettings, useLocalModel, useCookieModel} from "@/services/browserSettings.js";
-import {} from "@/services/browserSettings.js";
-import {MyVueXModel} from "@/models/MyVueXModel.js";
+import VueJsonPretty from "vue-json-pretty";
 import {useStore} from "vuex";
 import {Model} from "@app/models/Model.js";
+import {MyVueXModel} from "@/models/examples/MyVueXModel.js";
+import {LocalDataModel} from "@/models/examples/LocalDataModel.js";
+import {CookieDataModel} from "@/models/examples/CookieDataModel.js";
+import {SessionDataModel} from "@/models/examples/SessionDataModel.js";
+import {useBrowserSettings} from "@/services/browserSettings.js";
 
-const localModel = useLocalModel()
-const sessionModel = useSessionModel()
-const cookieModel = useCookieModel()
-const vuexModel = new MyVueXModel();
 const store = useStore()
+const vuexModel = new MyVueXModel()
+const localModel = new LocalDataModel()
+const cookieModel = new CookieDataModel()
+const sessionModel = new SessionDataModel();
 const model = new Model({
     theme: 'theme-name-333',
     fontsize: 33,
@@ -20,13 +23,12 @@ const model = new Model({
 })
 
 
-console.log('MODELS: cookieModel data', cookieModel.data());
-console.log('MODELS: localModel data', localModel.data());
-console.log('MODELS: sessionModel data', sessionModel.data());
-console.log('MODELS: vuexModel data', vuexModel.data());
-console.log('MODELS: model data', model.data());
 console.log('VUEX: store', store);
-
+console.log('MODELS: model data', model.data());
+console.log('MODELS: vuexModel data', vuexModel.data());
+console.log('MODELS: localModel data', localModel.data());
+console.log('MODELS: cookieModel data', cookieModel.data());
+console.log('MODELS: sessionModel data', sessionModel.data());
 
 </script>
 
@@ -36,13 +38,10 @@ console.log('VUEX: store', store);
             Settings sidebar
         </template>
 
-        Settings
-
-
+        Model examples
 
         <hr>
         <h4>CookieModel</h4>
-        {{ cookieModel.data() }}
         <br>
         <input v-model="cookieModel.data().fontsize">
         <br>
@@ -50,11 +49,11 @@ console.log('VUEX: store', store);
         <br>
         <input v-model="cookieModel.data().address.city">
         <br>
+        <vue-json-pretty :data="cookieModel.data()"/>
 
 
         <hr>
         <h4>LocalModel</h4>
-        {{ localModel.data() }}
         <br>
         <input v-model="localModel.data().fontsize">
         <br>
@@ -62,10 +61,10 @@ console.log('VUEX: store', store);
         <br>
         <input v-model="localModel.data().address.city">
         <br>
+        <vue-json-pretty :data="localModel.data()"/>
 
         <hr>
         <h4>SessionModel</h4>
-        {{ sessionModel.data() }}
         <br>
         <input v-model="sessionModel.data().fontsize">
         <br>
@@ -73,11 +72,11 @@ console.log('VUEX: store', store);
         <br>
         <input v-model="sessionModel.data().address.city">
         <br>
+        <vue-json-pretty :data="sessionModel.data()"/>
 
 
         <hr>
         <h4>VueXModel</h4>
-        {{ vuexModel.data() }}
         <br>
         <input v-model="vuexModel.data().fontsize">
         <br>
@@ -85,12 +84,12 @@ console.log('VUEX: store', store);
         <br>
         <input v-model="vuexModel.data().address.city">
         <br>
-        {{ store.state }}
+        <vue-json-pretty :data="vuexModel.data()"/>
+
 
 
         <hr>
         <h4>Model</h4>
-        {{ model.data() }}
         <br>
         <input v-model="model.data().fontsize">
         <br>
@@ -98,10 +97,13 @@ console.log('VUEX: store', store);
         <br>
         <input v-model="model.data().address.city">
         <br>
+        <vue-json-pretty :data="model.data()"/>
+
 
 
         <hr>
-        {{ store.state }}
+        <h4>Store</h4>
+        <vue-json-pretty :data="store.state"/>
 
 
     </default>
